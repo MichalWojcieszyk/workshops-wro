81ded80e504391a9d2d7a76ca8f41b849eeed3d7
Michal Wojcieszyk <mwojcieszyk@gmail.com>
2015-11-29 20:44:53 +0100

    Add before_action authentication and students crumb

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -27,6 +27,6 @@ class StudentsController < ApplicationController
   private
 
   def student_params
-    params.require(:student).permit(:first_name, :last_name, :subject_items_ids: [])
+    params.require(:student).permit(:first_name, :last_name, subject_items_ids: [])
   end
 end
