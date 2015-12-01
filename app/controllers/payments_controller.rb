fb9b02b954dbaea07d6f0fd3deb6ed10c6e9be34
Michal Wojcieszyk <mwojcieszyk@gmail.com>
2015-12-01 12:34:38 +0100

    Add methods to payment controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,10 +1,32 @@
 class PaymentsController < ApplicationController
+  expose(:students)
+  expose(:payment, attributes: :payment_params)
+  expose(:payments)
+
   def create
+  	if payment.save
+  		redirect_to payment_path(payment), notice: I18n.t('shared.created', resource: 'Payment')
+  	else
+  		render :new
+  	end
   end
 
   def update
+  	if payment.update
+  		redirect_to payment_path(payment), notice: I18n.t('shared.updated', resource: 'Payment')
+  	else
+  		render :edit
+  	end
   end
 
   def destroy
+  	payment.destroy
+  	redirect_to payments_path, notice: I18n.t('shared.deleted', resource: "Payment")
+  end
+
+  private
+
+  def payment_params
+  	params.require(:payment).permit(:sum, :date)
   end
 end
