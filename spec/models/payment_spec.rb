699c4552efd2db5a635cfedc1fcb298888689554
Michal Wojcieszyk <mwojcieszyk@gmail.com>
2015-12-03 12:53:00 +0100

    Create Payment model spec

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe Payment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Payment do
+	describe 'validations' do
+		it { is_expected.to validate_presence_of :sum }
+		it { is_expected.to validate_presence_of :date }
+		it { is_expected.to validate_presence_of :student }
+
+		context 'payment with negative sum' do
+			let(:student) { create :student }
+			subject { build :payment, student: student, sum: -100, date: Date.today }
+
+			it { is_expected.to_not be_valid }
+		end
+	end
+
+	describe 'database columns' do
+		it { should have_db_column :sum }
+		it { should have_db_column :date }
+		it { should have_db_column :student_id }
+	end
+
+	describe 'associations' do
+		it { is_expected.to belong_to :student }
+	end
 end
